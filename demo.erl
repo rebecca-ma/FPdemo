00adf2013d94dfd69bbd918b0918a9915aed3007
Rebecca Tang <rtang@caltech.edu>
2017-07-20 00:45:29 -0700

    Night changes

--- demo.erl
+++ demo.erl
@@ -1,5 +1,7 @@
 -module(demo).
--export([add/2, subtract/2, multiply/2, arithmetic/3, by_two/1, compose/2]).
+-export([add/2, subtract/2, multiply/2, arithmetic/3, recursive_sum/1,
+    by_two/1, compose/2,
+    do_arithmetic/3]).
 
 add(X, Y) ->
     X + Y.
@@ -16,6 +18,14 @@ arithmetic(subtract, X, Y) ->
     subtract(X, Y);
 arithmetic(multiply, X, Y) ->
     multiply(X, Y).
+
+recursive_sum([X]) ->
+    X;
+recursive_sum([X | Y]) ->
+    X + recursive_sum(Y).
+
+do_arithmetic(F, X, Y) ->
+    F(X, Y).
 
 by_two(F) ->
     fun (Val) -> F(Val, 2) end.
